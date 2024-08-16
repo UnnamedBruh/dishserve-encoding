76c4ee7ded38e206ff2254fe44288b4e89c85308
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-08-15 20:41:46 -0700

    Update and rename what-beats.js to what-wins.js

--- what-wins.js
+++ what-wins.js
@@ -1,7 +1,6 @@
 (function() {
-	const answers = [
-		{
-			object: "rock",
+	const answers = {
+		rock: {
 			choices: ["paper", "sheet", "boulder", "foil", "rag", "black hole", "lava", "volcano", "zero", "moss"],
 			answers: ["Well, duh. Paper covers rock.", "Well, duh. A sheet of paper covers rock as well as paper.", "A boulder is bigger than a rock, so... why not?", "Puh-lease. Foil wraps around a rock with ease. Hey, that rhymes!", "A rag covers a rock. Who wouldn't know that?", "Oh, come on. A black hole sucks up *everything*.", "Lava is made up with really hot rock, so... yeah.", "Throw a rock in a volcano, and what do you get? Melted rock.", "You cheesed this, didn't you... 0 rocks are nothing...", "Moss does cover rock, and rock can't do anything about it. No offense, nature.", "Sorry, but that doesn't beat rock."],
 			wrongChoices: {
@@ -17,8 +16,21 @@
 				money: "A penny's not strong against a rock, although it would be nice if money could be stronger than rock.",
 				water: "Water can't beat rock. It's not even strong enough to even destroy a rock.",
 				human: "A human being can't destroy a rock. If a human being bashes it on their skull, they could end up dying.",
-				tape: "I'm confident a tape can even get snapped if a rock falls heavily on the tape (middle of tape is mid-air)."
+				tape: "I'm confident a tape can even get snapped if a rock falls heavily on the tape (middle of tape is mid-air).",
+				"shrink ray": "Sorry buddy, but cheesing isn't a part of this game... or is it?"
 			}
 		}
 	]
+	const input = document.getElementById("answer")
+	let guessedBefore = new Set(["rock"]), currentGuess = "rock"
+	input.onkeydown = function(event) {
+		if (event.key == "Enter") {
+			input.value = String(input.value).toLowerCase().trim().replace(/(\s)+/, "$1")
+			if (!guessedBefore.has(input.value)) {
+				
+			} else {
+				alert("Sorry, but you can't repeat this.")
+			}
+		}
+	}
 })()
